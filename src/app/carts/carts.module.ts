4f98235b37542bf6c99c89c685bf94421bf93e53
ayaramadan63 <aya.ramadan1717@gmail.com>
2022-10-28 00:07:09 +0200

    E-Commerce

---
+++ src/app/carts/carts.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CartComponent } from './components/cart/cart.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    CartComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    FormsModule
+  ]
+})
+export class CartsModule { }
